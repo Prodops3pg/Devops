46e9c7875efbefd4b77a318555d14af4e1451940
Sushil Kumar <sushil.kumar@NDI-LAP-461.local>
2018-06-06 16:06:56 +0530

    Initial commit from sushil

---
+++ scripting.sh
@@ -0,0 +1,8 @@
+ e: filename == switch to the diffrent file 
+    bn: == back to the previous opened file
+       :vsp == split the vim into two windows
+          ctrl+www == swithc the splitted windows by :vsp
+	     j == for down 
+	        k == for up
+		   h == left
+		      l == right
