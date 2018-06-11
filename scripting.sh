99d64fba33223cd706a292f4a56a08132e50ec7a
prodopstpg <39800640+prodopstpg@users.noreply.github.com>
2018-06-11 20:26:58 +0530

    Delete scripting.sh

--- scripting.sh
+++
@@ -1,8 +0,0 @@
- e: filename == switch to the diffrent file 
-    bn: == back to the previous opened file
-       :vsp == split the vim into two windows
-          ctrl+www == swithc the splitted windows by :vsp
-	     j == for down 
-	        k == for up
-		   h == left
-		      l == right
